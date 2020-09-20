1fec170013e67089367a34deb253f7c5ce6ee580
caocaoya <798162716@qq.com>
2020-09-21 00:08:37 +0800

    test_ ok

--- Event_Manage/Event_Manage.c
+++ Event_Manage/Event_Manage.c
@@ -13,9 +13,17 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+#include "Event_Manage.h"
 
 Event_Manage_Type   Event_Mange;
 
+
+int  main()
+{
+	Event_Trig(&Event_Mange);	
+	
+	return 1;
+}
 /*******************************************************
   * 函数功能: 注册任务统计
   * 输入参数: 
@@ -25,56 +33,193 @@ Event_Manage_Type   Event_Mange;
 uint8_t Event_Register(char *name, Event_Manage_Type * event_manage, uint8_t *register_handle)
 {
     uint8_t status = REGISTER_NULL;
+    uint8_t count  = 0;
+    uint8_t flag_same_name = 0;
+    uint8_t flag_enable = 0, number_count = 0;
     
-    if(EVEN_MAX < event_manage -> Event_Tatol_Count)
+    if(EVENT_MAX < event_manage -> Event_Tatol_Count)
     {
         Even_Log("EVEN_Register over!\n\n");
         status = REGISTER_OVER_MAX;
     }
     //字符串的名字太长啦！
-    else if()
+    else if(strlen(name) >  EVENT_NAME_LENMAX)
     {
+       Even_Log("%s\n\n",name);
        status = REGISTER_TOO_LONG; 
     }
-    //名字已经注册
-    else if()
-    {
-       status = REGISTER_SAME_NAME; 
-    }
-    //标志位已经设置
-    else if()
-    {
-      status = REGISTER_ENABLE_BORE;  
-    } 
     else
     {
-        strcpy(name, event_manage -> Task_Param[event_manage -> Event_Tatol_Count].Task_Name);
-        Enable_Register_Table(event_manage -> Event_Tatol_Count, event_manage -> Event_Register);
-        register_handle = event_manage -> Event_Tatol_Count;
-        event_manage -> Event_Tatol_Count ++;
-        status = REGISTER_OK;
+        for(count = 0;count < (event_manage -> Event_Tatol_Count); count ++)
+        {
+            //检测名字是否重复
+           if( strstr(name, (event_manage -> Task_Param.Task_Param_Status[count].Task_Name) ))
+           {
+               flag_same_name = 1;
+               break;
+           }  
+           //标志位是空的！
+           if(!(flag_enable)  &&  !(Get_Bit_Status(&(event_manage -> Event_Register),count) ) )
+           {
+               number_count = count;
+               flag_enable = 1;
+           }               
+        }
+
+        //名字已经注册
+        if( flag_same_name )
+        {
+           Even_Log("name is register\n\n");
+           status = REGISTER_SAME_NAME; 
+        }
+        else
+        {
+            
+            if(flag_enable)
+            {
+                strcpy( event_manage -> Task_Param.Task_Param_Status[number_count].Task_Name, name);
+            
+                Enable_Register_Table(number_count, &(event_manage -> Event_Register));
+                
+                *register_handle = number_count;
+            }
+            else
+            {
+                strcpy(event_manage -> Task_Param.Task_Param_Status[event_manage -> Event_Tatol_Count].Task_Name, name);
+            
+                Enable_Register_Table(event_manage -> Event_Tatol_Count, &(event_manage -> Event_Register));
+
+                *register_handle = event_manage -> Event_Tatol_Count;  
+            }
+
+            event_manage -> Event_Tatol_Count ++;
+            //event_manage -> Task_All_Tatol = event_manage -> Event_Tatol_Count;
+            status = REGISTER_OK;   
+        }
     }
     
     return status;
 }
-
-
-uint8_t Enable_Register_Table(uint8_t bit_number, uint32_t data)
+/*******************************************************
+  * 函数功能: 注销任务统计
+  * 输入参数: 
+  * 返 回 值: 无
+  * 说    明：无
+*******************************************************/
+uint8_t Event_Unregister(Event_Manage_Type * event_manage, uint8_t register_handle)
 {
+    uint8_t status = 0;
     
+    if( Get_Bit_Status(&(event_manage -> Event_Register), register_handle) )
+    {
+        strset( event_manage -> Task_Param.Task_Param_Status[register_handle].Task_Name,0);
+        Disable_Register_Table(register_handle, &(event_manage -> Event_Register));
+        event_manage -> Event_Tatol_Count--;
+        status = REGISTER_OK; 
+    }
+    else
+    {
+        Even_Log("Event_Unregister %d is not Register.\n",register_handle);
+    }
+    
+
+
+    return status;
 }
 
-uint8_t Disable_Register_Table(uint8_t bit_number, uint32_t data)
+uint8_t Enable_Register_Table(uint8_t bit_number, uint32_t *data)
 {
+    uint32_t  enable_bit = 0x00000001;
     
+    if(bit_number > EVENT_MAX)
+    {
+        Even_Log("bit_number error\n\n");
+    }
+    else
+    {   
+        {
+            *data |= (enable_bit << bit_number);  
+        }
+        
+    }
 }
 
-uint8_t Even_Run_Task(void)
+uint8_t Disable_Register_Table(uint8_t bit_number, uint32_t *data)
 {
+    uint32_t  clear_bit = 0x00000001;
     
+    if(bit_number > EVENT_MAX)
+    {
+        Even_Log("bit_number error\n\n");
+    }
+    else
+    {
+        *data &= (~(clear_bit << bit_number)); 
+    }    
+}
+uint8_t Get_Bit_Status(uint32_t *bit_data, uint8_t bit_number)
+{
+    uint8_t status = 1;
+    
+    uint32_t clear_bit = 0x00000001;
+    uint32_t data = 0;
+    
+    data |= (1 << bit_number); 
+    
+    if( !(*bit_data & data) )
+    {
+        status = 0;
+    }
+
+    return status;
 }
 
+uint8_t Event_Task_Statistic(Event_Manage_Type * event_manage, uint8_t register_handle)
+{
+    if( !Get_Bit_Status(&(event_manage -> Event_Register), register_handle) )
+    {
+        event_manage -> Task_Param.Task_Param_Status[register_handle].Task_Run_Count++;
+    }
+    else
+    {
+        Even_Log("Event_Task_Statistic %d is not Register.\n",register_handle);
+    }
+    
+}
+uint8_t Even_Run_Task(Event_Manage_Type * event_manage,uint8_t register_handle)
+{
+    if( !Get_Bit_Status(&(event_manage -> Event_Register),register_handle) )
+    {
+        Enable_Register_Table(register_handle, &(event_manage -> Event_Register));
+    }
+    else
+    {
+        Even_Log("Even_Run_Task %d is not Register.\n",register_handle);
+    }
+}
 
+uint8_t Event_Trig(Event_Manage_Type * event_manage)
+{
+    uint32_t error_task_handle = 0;
+    
+    if( ((event_manage -> Event_Register) & (event_manage -> Event_Run_Status) == (event_manage -> Event_Register) ))
+    {
+        return 0;
+    }
+    else
+    {
+        error_task_handle = (event_manage -> Event_Register) ^ (event_manage -> Event_Run_Status);
+        
+        for(uint8_t count = 0;count < (event_manage -> Event_Tatol_Count); count ++)
+        {
+           //打印出错误的任务
+           if(Get_Bit_Status(&error_task_handle,count) )
+           {
+               Even_Log("Event_Trig %d is error.\n",count);
+           }               
+        }
+    }
+}
 
 
 
--- Event_Manage/Event_Manage.h
+++ Event_Manage/Event_Manage.h
@@ -3,57 +3,88 @@
 #ifndef __TASEK_RUN_H
 #define __TASEK_RUN_H
 
+#define ENABLE_LOG         1
+#define EVENT_MAX          32
 
-#define EVEN_MAX    32
-#define ENABLE_LOG  1
+#define EVENT_NAME_LENMAX  30
 
+#define uint8_t  unsigned char 
+#define uint16_t unsigned short
+#define uint32_t unsigned int
 
 #if ENABLE_LOG
-void Even_Log(char *str)
-{
-    printf("%s",str);
-}
+#define Even_Log printf
+// void Even_Log(char *str)
+// {
+    // printf("%s",str);
+// }
 #else
-void Even_Log(char *str)
-{
+#define Even_Log printf
+// void Even_Log(char *str)
+// {
     
-}  
+// }  
 #endif
 
-struct enum
+typedef struct enum
 {
     EVENT_OK,
     EVENT_ERROR
 }Status_Type;
 
-struct enum
+typedef struct enum
 {
     REGISTER_NULL,
     REGISTER_OK,
     REGISTER_TOO_LONG,
     REGISTER_SAME_NAME,
-    REGISTER_ENABLE_BORE,
     REGISTER_OVER_MAX
 }Register_Status_Type;
 
-struct 
+
+typedef struct 
 {
-   char      Task_Name[30];   //任务运行的名字。
+   char      Task_Name[EVENT_NAME_LENMAX];   //任务运行的名字。
    uint32_t  Task_Run_Count;  //运行的次数。
    
+}Task_Param_Type;
+
+typedef struct 
+{
+    uint32_t          Task_All_Tatol;
+    Task_Param_Type   Task_Param_Status[EVENT_MAX];
 }Manage_Param_Type;
 
-struct 
+   
+typedef struct 
 {
     uint8_t           Event_Tatol_Count; //注册的任务的总数。
     uint32_t          Event_Register;    //任务注册登记表。
     uint32_t          Event_Run_Status;  //已注册的任务运行状态。
-    uint32_t          Task_All_Tatol;
-    Manage_Param_Type Task_Param[EVEN_MAX];       //注册的任务的运行状态。
+
+    Manage_Param_Type Task_Param;       //注册的任务的运行状态。
 }Event_Manage_Type;
 
 
+uint8_t Event_Register(char *name, Event_Manage_Type * event_manage, uint8_t *register_handle);
+uint8_t Event_Unregister(Event_Manage_Type * event_manage, uint8_t register_handle);
+uint8_t Enable_Register_Table(uint8_t bit_number, uint32_t *data);
+uint8_t Disable_Register_Table(uint8_t bit_number, uint32_t *data);
+uint8_t Get_Bit_Status(uint32_t *bit_data, uint8_t bit_number);
+uint8_t Event_Task_Statistic(Event_Manage_Type * event_manage, uint8_t register_handle);
+uint8_t Even_Run_Task(uint8_t register_handle);
+uint8_t Event_Trig(Event_Manage_Type * event_manage);
 
 
 #endif
+
+
+
+
+
+
+
+
+
+
 
